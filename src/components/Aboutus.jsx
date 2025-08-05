a5ab6734815187a5da122d99b832cfe6a9405509
Arshad1213 <shaik.arshad.prs556@gmail.com>
2025-08-05 12:12:10 +0530

    Update Aboutus.jsx

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -4,8 +4,8 @@ import { Link, Outlet } from "react-router-dom";
 
 const AboutUs = () => {
   return (
-    <div className="abouts-container">
-      <h2>About Our Electronic Store</h2>
+    <div className="about-container">
+      <h2>About Our Electronic Store nothing</h2>
       <p>
         Welcome to our E-Commerce platform, your one-stop shop for the latest and greatest in electronics!
         We specialize in high-quality products, including computers, mobiles, laptops, and accessories.
